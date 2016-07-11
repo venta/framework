432a8f6d75ca7fd76e03d29e07a74e1a5f6f0e4d
Alexey Rehov <alexey.rehov@gmail.com>
2016-07-11 16:34:40 +0300

    Console kernel incomplete test added

---
+++ tests/ConsoleKernelTest.php
@@ -0,0 +1,34 @@
+<?php
+
+class ConsoleKernelTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testHandle()
+    {
+        // todo Unskip this test when appropriate callExtensionProvidersMethod will be available
+        $this->markTestSkipped('callExtensionProvidersMethod fallback required');
+
+        $app = $this->getMockBuilder(\Venta\Framework\Contracts\ApplicationContract::class)->getMock();
+        $app->method('version')->willReturn('1.0');
+        $app->method('bind');
+        $app->method('bootExtensionProviders');
+
+        $proxy->app = $app;
+
+        $kernel = new \Venta\Framework\Kernel\ConsoleKernel($proxy);
+        $result = $kernel->handle(
+            new \Symfony\Component\Console\Input\ArrayInput([]),
+            new \Symfony\Component\Console\Output\NullOutput
+        );
+        $this->assertSame(0, $result);
+    }
+
+    public function testTerminate()
+    {
+        $app = $this->getMockBuilder(\Venta\Framework\Contracts\ApplicationContract::class)->getMock();
+        $app->method('terminate');
+        $kernel = new \Venta\Framework\Kernel\ConsoleKernel($app);
+        $kernel->terminate();
+    }
+
+}
