a7cb4acc83202271d19c59d4a463a8585c5400e1
Vsevolods Mihailovs <cheelahim@gmail.com>
2016-07-18 16:18:54 +0300

    Introduce new framework structure

--- src/Venta/tests/bootstrap.php
+++ src/Venta/tests/bootstrap.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/abava/container/tests/StubClasses.php';
 
-(new \Dotenv\Loader(__DIR__.'/.env', false))->setEnvironmentVariable('APP_ENV', \Venta\Application::ENV_TEST);
+(new \Dotenv\Loader(__DIR__ . '/.env', false))->setEnvironmentVariable('APP_ENV', \Venta\Application::ENV_TEST);
 
 interface Router{
 
