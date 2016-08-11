928aed7b6d5f231303d2f684f2a82b65861e6765
Ilja Lapkovskis <ilja@scandi.com>
2016-08-11 13:06:50 +0300

    Add response helper methods for cookie management. Add plaintext cookie parser. Add tests

--- src/Abava/Http/Response.php
+++ src/Abava/Http/Response.php
@@ -4,6 +4,7 @@ namespace Abava\Http;
 
 use Abava\Http\Contract\Response as ResponseContract;
 use Abava\Http\Contract\Cookie as CookieContract;
+use MongoDB\BSON\Regex;
 use Zend\Diactoros\Response as BaseResponse;
 
 /**
@@ -15,17 +16,18 @@ class Response extends BaseResponse implements ResponseContract
 {
     use ResponseTrait;
 
-    public function addCookie(CookieContract $cookie): ResponseContract
+    public function addCookie(CookieContract $cookie)
     {
         return $this->addCookieToHeader($cookie);
     }
 
     /**
      * @param $cookies Cookie[]
-     * @return ResponseContract;
+     * @return ResponseContract|RequestContract;
      * @throws \InvalidArgumentException
      */
-    public function addCookies($cookies): ResponseContract
+
+    public function addCookies($cookies)
     {
         if (!is_array($cookies) && !$cookies instanceof \Traversable) {
             throw new \InvalidArgumentException('Array elements must implement Cookie contract');
@@ -44,5 +46,48 @@ class Response extends BaseResponse implements ResponseContract
     protected function addCookieToHeader(Cookie $cookie)
     {
         return $this->withAddedHeader('Set-Cookie', $cookie->asPlainText());
+    }
+
+    /**
+     * Get all cookies set to response
+     *
+     * @param $returnObject bool
+     * @return null|string[]|Cookie[]
+     */
+    public function getCookies(bool $returnObject = false)
+    {
+        if (!$this->getHeader('set-cookie')) {
+            return null;
+        }
+        if (false === $returnObject) {
+            return $this->getHeader('set-cookie');
+        }
+        $cookieObjects = array();
+        foreach ($this->getHeader('set-cookie') as $cookie) {
+            $cookieObjects[] = Cookie::createFromString($cookie);
+        }
+
+        return $cookieObjects;
+    }
+
+    /**
+     * Get a single cookie by name
+     *
+     * @param string $name
+     * @param bool $returnObject
+     * @return null|string[]
+     */
+    public function getCookie(string $name, bool $returnObject = false)
+    {
+        if (!$cookies = $this->getCookies()) {
+            return null;
+        }
+        $pattern = '/(^' . $name . '=.*)/';
+        $cookie = implode(preg_grep($pattern, $cookies));
+        if ($cookie == '') {
+            return null;
+        }
+
+        return $returnObject ? Cookie::createFromString($cookie) : $cookie;
     }
 }
