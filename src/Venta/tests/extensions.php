a728c9f94a2f690562e506e50b64ee30604fd3a8
Alexey Rehov <alexey.rehov@gmail.com>
2016-08-22 18:55:28 +0300

    KernelTest case added.

--- src/Venta/tests/extensions.php
+++ src/Venta/tests/extensions.php
@@ -1,14 +1,44 @@
 <?php
 
+use Abava\Container\Contract\Container;
+use Abava\Routing\Contract\Group;
+use Abava\Routing\Contract\Middleware\Collector as MiddlewareCollector;
+use Venta\Contract\ExtensionProvider\Bindings;
+use Venta\Contract\ExtensionProvider\Commands;
+use Venta\Contract\ExtensionProvider\Middlewares;
+use Venta\Contract\ExtensionProvider\Routes;
+
+class SampleExtension implements Routes, Middlewares, Bindings, Commands
+{
+    /**
+     * @inheritDoc
+     */
+    public function bindings(Container $container)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function commands(\Abava\Console\Contract\Collector $collector)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function middlewares(MiddlewareCollector $collector)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function routes(Group $collector)
+    {
+    }
+}
+
 return [
     SampleExtension::class
 ];
-
-class SampleExtension {
-
-    public function bindings($app) {}
-
-    public function errors($error_handler) {}
-
-    public function terminate($app) {}
-}
