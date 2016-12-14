af8342c5238e43393ea6ca0f165882193992edfe
Vsevolods Mihailovs <cheelahim@gmail.com>
2016-12-14 12:37:29 +0200

    Refactor Cookie specs.

--- src/Http/spec/CookieSpec.php
+++ src/Http/spec/CookieSpec.php
@@ -13,49 +13,14 @@ class CookieSpec extends ObjectBehavior
 {
     function let()
     {
-        $this->beConstructedWith('name', 'value', null, '/path', 'example.com', false, false);
+        $this->beConstructedWith('name');
     }
 
-    function it_can_be_http_only()
-    {
-        $this->shouldNotBeHttpOnly();
-        $cookie = $this->httpOnly();
-        $cookie->shouldBeHttpOnly();
-        $this->shouldNotBeHttpOnly();
-    }
-
-    function it_can_be_secured()
-    {
-        $this->shouldNotBeSecure();
-        $cookie = $this->secured();
-        $cookie->shouldBeSecure();
-        $this->shouldNotBeSecure();
-    }
-
-    function it_can_render_empty_value()
+    function it_can_be_deleted()
     {
         $deleted = $this->withValue('');
         $deleted->__toString()->shouldContain('deleted');
         $deleted->__toString()->shouldContain('01-Jan-1970');
-    }
-
-    function it_can_render_to_setcookie_header_string()
-    {
-        $this->__toString()->shouldContain('name=value');
-        $this->__toString()->shouldContain('path=/path');
-        $this->__toString()->shouldContain('domain=example.com');
-        $this->__toString()->shouldNotContain('expires');
-        $this->__toString()->shouldNotContain('secure');
-        $this->__toString()->shouldNotContain('httponly');
-    }
-
-    function it_can_render_with_secure_httponly_and_expire()
-    {
-        $now = new DateTime('now', new DateTimeZone('GMT+0000'));
-        $cookie = $this->withExpires($now)->secured()->httpOnly();
-        $cookie->__toString()->shouldContain('expires=' . $now->format(DateTime::COOKIE));
-        $cookie->__toString()->shouldContain('secure');
-        $cookie->__toString()->shouldContain('httponly');
     }
 
     function it_can_urlencode_value()
@@ -76,51 +41,67 @@ class CookieSpec extends ObjectBehavior
         $this->shouldThrow(InvalidArgumentException::class)->duringInstantiation();
     }
 
-    function it_has_immutable_domain()
+    function it_is_immutable()
     {
-        $this->domain()->shouldBe('example.com');
-        $cookie = $this->withDomain('domain');
-        $cookie->domain()->shouldBe('domain');
-        $this->domain()->shouldBe('example.com');
-    }
-
-    function it_has_immutable_expires_datetime()
-    {
-        $this->expires()->shouldBeNull();
         $dateTime = new DateTime();
-        $cookie = $this->withExpires($dateTime);
-        $cookie->expires()->shouldBeAnInstanceOf(DateTimeImmutable::class);
-        $cookie->expires()->shouldBeLike($dateTime);
-        $cookie->expires()->shouldNotBeLike($dateTime->setTimestamp(1));
-    }
+        $cookie = $this
+            ->withPath('/new-path')
+            ->withValue('new-value')
+            ->withDomain('domain.com')
+            ->withExpiration($dateTime)
+            ->httpOnly()
+            ->secured();
 
-    function it_has_immutable_path()
-    {
-        $this->path()->shouldBe('/path');
-        $cookie = $this->withPath('/new-path');
         $cookie->path()->shouldBe('/new-path');
-        $this->path()->shouldBe('/path');
-    }
+        $cookie->value()->shouldBe('new-value');
+        $cookie->domain()->shouldBe('domain.com');
+        $cookie->expiration()->shouldBeAnInstanceOf(DateTimeImmutable::class);
+        $cookie->expiration()->shouldBeLike($dateTime);
+        $cookie->expiration()->shouldNotBeLike($dateTime->setTimestamp(1));
+        $cookie->shouldBeHttpOnly();
+        $cookie->shouldBeSecure();
 
-    function it_has_immutable_value()
-    {
-        $this->value()->shouldBe('value');
-        $cookie = $this->withValue('new value');
-        $cookie->value()->shouldBe('new value');
-        $this->value()->shouldBe('value');
+
+        $this->path()->shouldNotBe('/new-path');
+        $this->value()->shouldNotBe('/new-value');
+        $this->domain()->shouldNotBe('domain.com');
+        $this->expiration()->shouldNotBeLike($dateTime);
+        $this->shouldNotBeHttpOnly();
+        $this->shouldNotBeSecure();
+
     }
 
     function it_is_initializable()
     {
-        //$this->beConstructedWith('name', 'value', null, '/path', 'example.com', false, false);
+        $this->beConstructedWith('name', 'value', null, '/path', 'example.com', false, false);
+
         $this->shouldImplement(CookieContract::class);
         $this->name()->shouldBe('name');
         $this->value()->shouldBe('value');
-        $this->expires()->shouldBeNull();
+        $this->expiration()->shouldBeNull();
         $this->path()->shouldBe('/path');
         $this->domain()->shouldBe('example.com');
         $this->shouldNotBeSecure();
         $this->shouldNotBeHttpOnly();
+    }
+
+    function it_renders_correct_setcookie_header_value()
+    {
+        $this->beConstructedWith('name', 'value', null, '/path', 'example.com', false, false);
+
+        $this->__toString()->shouldContain('name=value');
+        $this->__toString()->shouldContain('path=/path');
+        $this->__toString()->shouldContain('domain=example.com');
+        $this->__toString()->shouldNotContain('expires');
+        $this->__toString()->shouldNotContain('secure');
+        $this->__toString()->shouldNotContain('httponly');
+
+        $now = new DateTime('now', new DateTimeZone('GMT+0000'));
+        $cookie = $this->withExpiration($now)->secured()->httpOnly();
+        $cookie->__toString()->shouldContain('expires=' . $now->format(DateTime::COOKIE));
+        $cookie->__toString()->shouldContain('secure');
+        $cookie->__toString()->shouldContain('httponly');
+
     }
 
 }
