d44e7d68d968bf51d884cd304b04a0136842f76a
Konstantins Zukovs <konstantins@majaslapa.lv>
2016-12-25 17:54:12 +0200

    Loading ENV variables

--- src/Framework/Kernel/Bootstrap/EnvironmentDetection.php
+++ src/Framework/Kernel/Bootstrap/EnvironmentDetection.php
@@ -21,6 +21,8 @@ final class EnvironmentDetection extends AbstractKernelBootstrap
         $filePath = rtrim($this->kernel()->rootPath(), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . '.env';
 
         $envLoader = new Loader($filePath);
+        $envLoader->load();
+
         if ($this->kernel()->isCli()) {
             // todo: Load different env fire for each environment.
             $env = (new ArgvInput)->getParameterOption(['--env', '-e']);
