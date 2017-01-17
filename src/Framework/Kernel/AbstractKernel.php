73381476c7f6922380c018c3d28c6b4e089ef020
Alexey Rehov <alexey.rehov@gmail.com>
2017-01-17 19:20:35 +0200

    Revert "ServiceProvider bootstrapping replaced with array_map calls."
    This reverts commit 4824e67c30a7c15463ad563c820323e2695d9bf8.

--- src/Framework/Kernel/AbstractKernel.php
+++ src/Framework/Kernel/AbstractKernel.php
@@ -37,6 +37,11 @@ abstract class AbstractKernel implements Kernel
     protected $containerClass = \Venta\Container\MutableContainer::class;
 
     /**
+     * @var ServiceProvider[]
+     */
+    private $providers = [];
+
+    /**
      * @inheritDoc
      */
     public function boot(): Container
@@ -51,20 +56,20 @@ abstract class AbstractKernel implements Kernel
         $config = new MutableConfig($appConfig);
         $container->bind(Config::class, new ConfigProxy($config));
 
-        array_map(function(AbstractServiceProvider $provider) use ($config, $appConfig) {
-            $provider->boot();
-            $config->merge($appConfig);
-
-            return $provider;
-        }, array_map(function ($providerClass) use ($config, $appConfig, $container) {
+        // Here we initializing service providers and collect container bindings.
+        foreach ($this->registerServiceProviders() as $providerClass) {
             $this->ensureServiceProvider($providerClass);
-            /** @var AbstractServiceProvider|ServiceProvider $provider */
-            $provider = new $providerClass($container->get(Container::class), $config);
+            /** @var AbstractServiceProvider $provider */
+            $this->providers[] = $provider = new $providerClass($container->get(Container::class), $config);
             $provider->bind($container);
             $config->merge($appConfig);
+        }
 
-            return $provider;
-        }, $this->registerServiceProviders()));
+        // Booting service providers on by one allows
+        foreach ($this->providers as $provider) {
+            $provider->boot();
+            $config->merge($appConfig);
+        }
 
         return $container->get(Container::class);
     }
