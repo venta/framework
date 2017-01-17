ac3ce9b3f61d8534f9b4f666d7634399072ad97b
Alexey Rehov <alexey.rehov@gmail.com>
2017-01-17 17:57:45 +0200

    Response emitter now respects headers_sent() value and doesn't inject Content-Length header.

--- src/Http/spec/ResponseEmitterSpec.php
+++ src/Http/spec/ResponseEmitterSpec.php
@@ -2,11 +2,77 @@
 
 namespace spec\Venta\Http;
 
+use phpmock\prophecy\PHPProphet;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ProphecyInterface;
 use Venta\Contracts\Http\ResponseEmitter;
+use Zend\Diactoros\Response;
 
 class ResponseEmitterSpec extends ObjectBehavior
 {
+    /**
+     * @var ProphecyInterface
+     */
+    protected $prophecy;
+
+    /**
+     * @var PHPProphet
+     */
+    protected $prophet;
+
+    function let()
+    {
+        $this->prophet = new PHPProphet();
+        $this->prophecy = $this->prophet->prophesize('Zend\Diactoros\Response');
+    }
+
+    function it_doesnt_emit_headers_if_already_sent()
+    {
+        $prophecy = $this->prophet->prophesize('Zend\Diactoros\Response');
+        $prophecy->header(Argument::cetera())->shouldNotBeCalled();
+        $prophecy->reveal();
+
+        $prophecy = $this->prophet->prophesize('Venta\Http');
+        $prophecy->headers_sent()->willReturn(true);
+        $prophecy->reveal();
+
+        $this->emit(new Response('php://memory', 200, ['header' => 'value']));
+        $this->prophet->checkPredictions();
+    }
+
+    function it_emits_body()
+    {
+        $prophecy = $this->prophet->prophesize('Venta\Http');
+        $prophecy->headers_sent()->willReturn(true);
+        $prophecy->reveal();
+
+        $response = new Response('php://memory', 200, ['header' => 'value']);
+        $body = "Hi there, I'm Venta!";
+        $response->getBody()->write($body);
+        ob_start();
+        $this->emit($response);
+        assert(ob_get_contents() === $body);
+        ob_end_clean();
+        $this->prophet->checkPredictions();
+    }
+
+    function it_emits_headers_and_status()
+    {
+        $prophecy = $this->prophet->prophesize('Zend\Diactoros\Response');
+        $prophecy->header(Argument::that(function ($header) {
+            return in_array($header, ['Header: value', 'HTTP/1.1 200 OK']);
+        }), Argument::cetera())->shouldBeCalled();
+        $prophecy->reveal();
+
+        $prophecy = $this->prophet->prophesize('Venta\Http');
+        $prophecy->headers_sent()->willReturn(false);
+        $prophecy->reveal();
+
+        $this->emit(new Response('php://memory', 200, ['header' => 'value']));
+        $this->prophet->checkPredictions();
+    }
+
     function it_is_initializable()
     {
         $this->shouldImplement(ResponseEmitter::class);
--- src/Http/src/ResponseEmitter.php
+++ src/Http/src/ResponseEmitter.php
@@ -2,14 +2,46 @@
 
 namespace Venta\Http;
 
+use Psr\Http\Message\ResponseInterface;
 use Venta\Contracts\Http\ResponseEmitter as ResponseEmitterContract;
 use Zend\Diactoros\Response\SapiEmitter;
+use Zend\Diactoros\Response\SapiEmitterTrait;
 
 /**
  * Class Emitter
  *
  * @package Venta\Http
  */
-class ResponseEmitter extends SapiEmitter implements ResponseEmitterContract
+final class ResponseEmitter extends SapiEmitter implements ResponseEmitterContract
 {
+    use SapiEmitterTrait;
+
+    /**
+     * Emits a response for a PHP SAPI environment.
+     *
+     * Emits the status line and headers via the header() function, and the
+     * body content via the output buffer.
+     *
+     * @param ResponseInterface $response
+     * @param null|int $maxBufferLevel Maximum output buffering level to unwrap.
+     */
+    public function emit(ResponseInterface $response, $maxBufferLevel = null)
+    {
+        if (!headers_sent()) {
+            $this->emitStatusLine($response);
+            $this->emitHeaders($response);
+        }
+        $this->flush($maxBufferLevel);
+        $this->emitBody($response);
+    }
+
+    /**
+     * Emit the message body.
+     *
+     * @param ResponseInterface $response
+     */
+    private function emitBody(ResponseInterface $response)
+    {
+        echo $response->getBody();
+    }
 }
